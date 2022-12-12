fceec9e4b75dd57a29fd8f751696a6bf007b826b
Charles Green <cg@simplycubed.com>
2022-12-12 09:42:34 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Fireflow
+
+This project contains open source Firebase and Webflow integrations.
+
+
+## License
+
+This project is licensed under Apache 2.0.  
+Full license text is available in [LICENSE](LICENSE).
---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  projects: [],
+  testPathIgnorePatterns: [
+    ".*/bin/",
+    ".*/lib/",
+    // Ignoring otherwise tests duplicate due to Jest `projects`
+    ".*/__tests__/.*.ts",
+  ],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/node_modules/**",
+    "!**/test-data/**",
+  ],
+  maxConcurrency: 10,
+};
