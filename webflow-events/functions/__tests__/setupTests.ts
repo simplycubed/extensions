a0255b6c6bdcd749314b5e1e22f4b4abf03387c2
Charles Green <cg@simplycubed.com>
2022-12-22 21:41:13 +0900

    Rename webflow-membersips to webflow-events

--- webflow-events/functions/__tests__/setupTests.ts
+++ webflow-events/functions/__tests__/setupTests.ts
@@ -3,13 +3,13 @@ import path from "path";
 (async function () {
   const $ = path.resolve(
     __dirname,
-    `../../../_emulator/extensions/webflow-memberships.env.local`
+    `../../../_emulator/extensions/webflow-events.env.local`
   );
 
   require("dotenv").config({
     path: path.resolve(
       __dirname,
-      `../../../_emulator/extensions/webflow-memberships.env.local`
+      `../../../_emulator/extensions/webflow-events.env.local`
     ),
   });
 })();
