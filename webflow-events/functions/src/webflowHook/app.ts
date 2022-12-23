72578c6db47d4cb5bb32e700899f34a73841d0f9
Anas Muhammed <anas.mu2022@gmail.com>
2022-12-23 19:28:37 +0400

    feat: add handlers

--- webflow-events/functions/src/webflowHook/app.ts
+++ webflow-events/functions/src/webflowHook/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import Webflow from "webflow-api";
 import * as logger from "../logger";
-import { handleMembershipsUserAccountAdded } from "./handler";
+import * as handlers from "./handler";
 import { firstoreDb, storage } from "../init";
 import config from "../config";
 import { getFunctionBaseUrl } from "../helpers";
@@ -76,8 +76,12 @@ webhookApp.post(
   validateWebflowSignatureMw,
   async (req, res) => {
     logger.info("payload", req.body);
-    await handleMembershipsUserAccountAdded(firstoreDb, storage, req.body);
-    res.status(200).json({ status: `done` });
+    await handlers.handleMembershipsUserAccountAdded(
+      firstoreDb,
+      storage,
+      req.body
+    );
+    res.status(200).json({ status: `success` });
   }
 );
 
@@ -86,7 +90,12 @@ webhookApp.post(
   validateWebflowSignatureMw,
   async (req, res) => {
     logger.info("payload", req.body);
-    res.status(500).json({ message: `not implemented` });
+    await handlers.handleMembershipsUserAccountUpdated(
+      firstoreDb,
+      storage,
+      req.body
+    );
+    res.status(200).json({ status: `success` });
   }
 );
 
@@ -95,7 +104,8 @@ webhookApp.post(
   validateWebflowSignatureMw,
   async (req, res) => {
     logger.info("payload", req.body);
-    res.status(500).json({ message: `not implemented` });
+    await handlers.handleEcommNewOrder(firstoreDb, storage, req.body);
+    res.status(200).json({ status: `success` });
   }
 );
 
@@ -104,7 +114,8 @@ webhookApp.post(
   validateWebflowSignatureMw,
   async (req, res) => {
     logger.info("payload", req.body);
-    res.status(500).json({ message: `not implemented` });
+    await handlers.handleEcommOrderUpdated(firstoreDb, storage, req.body);
+    res.status(200).json({ status: `success` });
   }
 );
 
@@ -113,7 +124,8 @@ webhookApp.post(
   validateWebflowSignatureMw,
   async (req, res) => {
     logger.info("payload", req.body);
-    res.status(500).json({ message: `not implemented` });
+    await handlers.handleEcommInventoryChanged(firstoreDb, storage, req.body);
+    res.status(200).json({ status: `success` });
   }
 );
 
@@ -122,7 +134,8 @@ webhookApp.post(
   validateWebflowSignatureMw,
   async (req, res) => {
     logger.info("payload", req.body);
-    res.status(500).json({ message: `not implemented` });
+    await handlers.handleCollectionItemCreated(firstoreDb, storage, req.body);
+    res.status(200).json({ status: `success` });
   }
 );
 
@@ -131,7 +144,8 @@ webhookApp.post(
   validateWebflowSignatureMw,
   async (req, res) => {
     logger.info("payload", req.body);
-    res.status(500).json({ message: `not implemented` });
+    await handlers.handleCollectionItemChanged(firstoreDb, storage, req.body);
+    res.status(200).json({ status: `success` });
   }
 );
 
@@ -140,11 +154,13 @@ webhookApp.post(
   validateWebflowSignatureMw,
   async (req, res) => {
     logger.info("payload", req.body);
-    res.status(500).json({ message: `not implemented` });
+    await handlers.handleCollectionItemDeleted(firstoreDb, storage, req.body);
+    res.status(200).json({ status: `success` });
   }
 );
 
 webhookApp.post("/collectionItemUnpublished", async (req, res) => {
   logger.info("payload", req.body);
-  res.status(500).json({ message: `not implemented` });
+  await handlers.handleCollectionItemUnpublished(firstoreDb, storage, req.body);
+  res.status(200).json({ status: `success` });
 });
--- webflow-events/functions/src/webflowHook/handler.ts
+++ webflow-events/functions/src/webflowHook/handler.ts
@@ -1,27 +1,282 @@
 import { firestore, storage } from "firebase-admin";
 import config from "../config";
-import { UserAccountAddedPayload } from "./types";
+import {
+  CollectionItemChanged,
+  CollectionItemCreated,
+  CollectionItemDeleted,
+  CollectionItemUnpublished,
+  EcommInventoryChanged,
+  EcommNewOrder,
+  EcommOrderChanged,
+  UserAccountAddedPayload,
+  UserAccountUpdatedPayload,
+} from "./types";
+
+async function writeUserToFirestore(
+  db: firestore.Firestore,
+  payload: UserAccountAddedPayload
+) {
+  return db
+    .collection(config.userFirestorePath)
+    .doc(payload._id)
+    .set(payload, { merge: true });
+}
+
+async function writeOrderToFirestore(
+  db: firestore.Firestore,
+  payload: EcommNewOrder | EcommOrderChanged
+) {
+  return db
+    .collection(config.orderFirestorePath)
+    .doc(payload.orderId)
+    .set(payload, { merge: true });
+}
+
+async function writeInventoryToFirestore(
+  db: firestore.Firestore,
+  payload: EcommInventoryChanged
+) {
+  return db
+    .collection(config.inventoryFirestorePath)
+    .doc(payload._id)
+    .set(payload, { merge: true });
+}
+
+async function writeCollectionItemToFirestore(
+  db: firestore.Firestore,
+  payload: CollectionItemCreated
+) {
+  return db
+    .collection(config.collectionItemFirestorePath)
+    .doc(payload._id)
+    .set(payload, { merge: true });
+}
+
+async function deleteCollectionItemFromFirestore(
+  db: firestore.Firestore,
+  _id: string
+) {
+  return db.collection(config.collectionItemFirestorePath).doc(_id).delete();
+}
+
+async function writeInventoryToStorage(
+  storage: storage.Storage,
+  payload: EcommInventoryChanged
+) {
+  const fileContents = JSON.stringify(payload);
+  return storage
+    .bucket(config.storageBucketDefault)
+    .file(`${config.inventoryStoragePath}/${payload._id}.json`)
+    .save(fileContents, { validation: false });
+}
+
+async function writeUserToStorage(
+  storage: storage.Storage,
+  payload: UserAccountAddedPayload
+) {
+  const fileContents = JSON.stringify(payload);
+  return storage
+    .bucket(config.storageBucketDefault)
+    .file(`${config.userStoragePath}/${payload._id}.json`)
+    .save(fileContents, { validation: false });
+}
+
+async function writeOrderToStorage(
+  storage: storage.Storage,
+  payload: EcommNewOrder | EcommOrderChanged
+) {
+  const fileContents = JSON.stringify(payload);
+  return storage
+    .bucket(config.storageBucketDefault)
+    .file(`${config.orderStoragePath}/${payload.orderId}.json`)
+    .save(fileContents, { validation: false });
+}
+
+async function writeCollectionItemToStorage(
+  storage: storage.Storage,
+  payload: CollectionItemCreated | CollectionItemChanged
+) {
+  const fileContents = JSON.stringify(payload);
+  return storage
+    .bucket(config.storageBucketDefault)
+    .file(`${config.collectionItemStoragePath}/${payload._id}.json`)
+    .save(fileContents, { validation: false });
+}
+
+async function deleteCollectionItemFromStorage(
+  storage: storage.Storage,
+  _id: string
+) {
+  return storage
+    .bucket(config.storageBucketDefault)
+    .file(`${config.collectionItemStoragePath}/${_id}.json`)
+    .delete();
+}
 
 /*
- * The onMembershipsUserAccountAdded function creates new webflow user in firestore and then
- * returns a success message.
+ * The handleMembershipsUserAccountAdded function creates new webflow user in firestore, storage and database.
  */
 export const handleMembershipsUserAccountAdded = async (
   db: firestore.Firestore,
   storage: storage.Storage,
   payload: UserAccountAddedPayload
 ) => {
-  await db
-    .collection(config.firestoreUserCollectionName)
-    .doc(payload._id)
-    .set(payload, { merge: true });
+  if (config.userFirestorePath) {
+    // sync to firestore
+    await writeUserToFirestore(db, payload);
+  }
 
-  if (config.storageUserPath) {
+  if (config.userStoragePath) {
     // sync to storage
-    const fileContents = JSON.stringify(payload);
-    await storage
-      .bucket(config.storageBucketDefault)
-      .file(`${config.storageUserPath}/${payload._id}.json`)
-      .save(fileContents, { validation: false });
+    await writeUserToStorage(storage, payload);
+  }
+};
+
+/*
+ * The onMembershipsUserAccountUpdated function creates new webflow user in firestore, storage and database.
+ */
+export const handleMembershipsUserAccountUpdated = async (
+  db: firestore.Firestore,
+  storage: storage.Storage,
+  payload: UserAccountUpdatedPayload
+) => {
+  if (config.userFirestorePath) {
+    // sync to firestore
+    await writeUserToFirestore(db, payload);
+  }
+
+  if (config.userStoragePath) {
+    // sync to storage
+    await writeUserToStorage(storage, payload);
+  }
+};
+
+/*
+ * The handleEcommNewOrder function creates new webflow order in firestore, storage and database.
+ */
+export const handleEcommNewOrder = async (
+  db: firestore.Firestore,
+  storage: storage.Storage,
+  payload: EcommNewOrder
+) => {
+  if (config.userFirestorePath) {
+    // sync to firestore
+    await writeOrderToFirestore(db, payload);
+  }
+
+  if (config.userStoragePath) {
+    // sync to storage
+    await writeOrderToStorage(storage, payload);
+  }
+};
+
+/*
+ * The handleEcommOrderUpdated function creates new webflow order in firestore, storage and database.
+ */
+export const handleEcommOrderUpdated = async (
+  db: firestore.Firestore,
+  storage: storage.Storage,
+  payload: EcommNewOrder
+) => {
+  if (config.orderFirestorePath) {
+    // sync to firestore
+    await writeOrderToFirestore(db, payload);
+  }
+
+  if (config.orderStoragePath) {
+    // sync to storage
+    await writeOrderToStorage(storage, payload);
+  }
+};
+
+/*
+ * The handleEcommInventoryChanged function creates new webflow order in firestore, storage and database.
+ */
+export const handleEcommInventoryChanged = async (
+  db: firestore.Firestore,
+  storage: storage.Storage,
+  payload: EcommInventoryChanged
+) => {
+  if (config.inventoryFirestorePath) {
+    // sync to firestore
+    await writeInventoryToFirestore(db, payload);
+  }
+
+  if (config.inventoryStoragePath) {
+    // sync to storage
+    await writeInventoryToStorage(storage, payload);
+  }
+};
+
+export const handleCollectionItemCreated = async (
+  db: firestore.Firestore,
+  storage: storage.Storage,
+  payload: CollectionItemCreated
+) => {
+  if (config.collectionItemFirestorePath) {
+    // sync to firestore
+    await writeCollectionItemToFirestore(db, payload);
+  }
+
+  if (config.collectionItemStoragePath) {
+    // sync to storage
+    await writeCollectionItemToStorage(storage, payload);
+  }
+};
+
+/*
+ * The handleCollectionItemChanged function creates new webflow order in firestore, storage and database.
+ */
+export const handleCollectionItemChanged = async (
+  db: firestore.Firestore,
+  storage: storage.Storage,
+  payload: CollectionItemChanged
+) => {
+  if (config.collectionItemFirestorePath) {
+    // sync to firestore
+    await writeCollectionItemToFirestore(db, payload);
+  }
+
+  if (config.collectionItemStoragePath) {
+    // sync to storage
+    await writeCollectionItemToStorage(storage, payload);
+  }
+};
+
+/*
+ * The handleCollectionItemDeleted function creates new webflow order in firestore, storage and database.
+ */
+export const handleCollectionItemDeleted = async (
+  db: firestore.Firestore,
+  storage: storage.Storage,
+  payload: CollectionItemDeleted
+) => {
+  if (config.collectionItemFirestorePath) {
+    // delete from firestore
+    await deleteCollectionItemFromFirestore(db, payload.itemId);
+  }
+
+  if (config.collectionItemStoragePath) {
+    // delete from storage
+    await deleteCollectionItemFromStorage(storage, payload.itemId);
+  }
+};
+
+/*
+ * The handleCollectionItemUnpublished function creates new webflow order in firestore, storage and database.
+ */
+export const handleCollectionItemUnpublished = async (
+  db: firestore.Firestore,
+  storage: storage.Storage,
+  payload: CollectionItemUnpublished
+) => {
+  if (config.collectionItemFirestorePath) {
+    // delete from firestore
+    await deleteCollectionItemFromFirestore(db, payload.itemId);
+  }
+
+  if (config.collectionItemStoragePath) {
+    // delete from storage
+    await deleteCollectionItemFromStorage(storage, payload.itemId);
   }
 };
