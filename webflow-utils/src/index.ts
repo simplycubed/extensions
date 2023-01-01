6d0490a1abc7a9dbe91669888fe760d31fc9cb3d
Anas Muhammed <anas.mu2022@gmail.com>
2023-01-01 23:01:49 +0400

    chore: refactor auth endpoints

--- webflow-utils/src/index.ts
+++ webflow-utils/src/index.ts
@@ -2,3 +2,4 @@ export { createValidateWebflowSignatureMw } from "./validateWebflowSignature";
 export * from "./types";
 export * from "./logger";
 export * from "./helpers";
+export * from "./auth";
