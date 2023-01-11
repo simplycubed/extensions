56390a70c8c88774bdc6d2280bc6acd3717ecffb
Anas Muhammed <anas.mu2022@gmail.com>
2023-01-11 23:57:22 +0400

    chore: add jest config for coverage

--- webflow-events-storage/functions/jest.config.js
+++ webflow-events-storage/functions/jest.config.js
@@ -3,8 +3,14 @@ const packageJson = require("./package.json");
 module.exports = {
   name: packageJson.name,
   displayName: packageJson.name,
-  rootDir: "./__tests__",
   preset: "ts-jest",
   testMatch: ["**/__tests__/*.test.ts"],
+  collectCoverageFrom: [
+    "**/*.{ts,tsx}",
+    "!**/node_modules/**",
+    "!**/test-data/**",
+  ],
+  collectCoverage: true,
+  coverageReporters: ["json", "html"],
   setupFilesAfterEnv: [],
 };
